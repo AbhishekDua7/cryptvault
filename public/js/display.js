26e7ea52b8dcd1a2a0e6ba2d72fcc787afb70653
VIVEK AGRAWAL <agrawal.vivek2017@gmail.com>
2023-04-20 12:25:45 -0500

    Storing data in properties file

--- public/js/display.js
+++ public/js/display.js
@@ -1,6 +1,13 @@
+
+
+
+
+
+
 var ipc = require('electron').ipcRenderer;
 var file = [];
 console.log('Running Diplay.js');
+const fs = require('fs');
 function printFiles(folder)
 {
     var i
@@ -50,6 +57,171 @@ function printFiles(folder)
         // });
     }
 }
+
+let tags = [];
+let values = [];
+
+function addProperty() {
+    const tagInput = document.getElementById("tagInput").value;
+    const valueInput = document.getElementById("valueInput").value;
+
+    tags.push(tagInput);
+    values.push(valueInput);
+
+    const propertiesTextArea = document.getElementById("propertiesTextArea");
+    propertiesTextArea.value += `${tagInput}=${valueInput}\n`;
+
+    document.getElementById("tagInput").value = "";
+    document.getElementById("valueInput").value = "";
+}
+
+function createProperties() {
+    console.log("tags: "+ tags);
+    console.log("Values: "+ values);
+    createPropertiesFile(tags, values);
+}
+
+
+// function createPropertiesFile(tags, values) {
+//     // implementation here
+//     // create properties object from array lists
+// const properties = {};
+// for (let i = 0; i < tags.length; i++) {
+//   properties[tags[i]] = values[i];
+// }
+
+// // write properties object to file
+// fs.writeFile('data.properties', formatProperties(properties), function (err) {
+//   if (err) throw err;
+//   console.log('Properties file created!');
+// });
+
+// // function to format properties object into string
+
+// }
+
+function createProperties() {
+    let tags = [];
+    let values = [];
+  
+    // check if data.properties file already exists
+    if (fs.existsSync('data.properties')) {
+      // read current properties from file
+      const currentProperties = fs.readFileSync('data.properties', 'utf8');
+    
+      // parse current properties into object
+      const parsedProperties = parseProperties(currentProperties);
+  
+      // populate tags and values arrays with existing properties
+      for (let key in parsedProperties) {
+        tags.push(key);
+        values.push(parsedProperties[key]);
+      }
+  
+      // update text area with existing properties
+      const propertiesTextArea = document.getElementById("propertiesTextArea");
+      propertiesTextArea.value = formatProperties(parsedProperties);
+    }
+  
+    // add new properties from input fields
+    const tagInput = document.getElementById("tagInput").value;
+    const valueInput = document.getElementById("valueInput").value;
+    if (tagInput && valueInput) {
+      tags.push(tagInput);
+      values.push(valueInput);
+    }
+  
+    // create properties object from arrays
+    const properties = {};
+    for (let i = 0; i < tags.length; i++) {
+      properties[tags[i]] = values[i];
+    }
+  
+    // write properties object to file
+    fs.writeFile('data.properties', formatProperties(properties), function (err) {
+      if (err) throw err;
+      console.log('Properties file created or updated!');
+    });
+  
+    // clear input fields
+    document.getElementById("tagInput").value = "";
+    document.getElementById("valueInput").value = "";
+  }
+  
+
+function formatProperties(properties) {
+    let output = '';
+    for (let key in properties) {
+      output += key + '=' + properties[key] + '\n';
+    }
+    return output;
+  }
+
+function updateProperties() {
+    const propertiesTextArea = document.getElementById("propertiesTextArea");
+    const lines = propertiesTextArea.value.split("\n");
+
+    // extract tags and values from lines
+    const tags = [];
+    const values = [];
+    for (let i = 0; i < lines.length; i++) {
+        const parts = lines[i].split("=");
+        if (parts.length >= 2) {
+            const tag = parts[0].trim();
+            const value = parts[1].trim();
+            tags.push(tag);
+            values.push(value);
+        }
+    }
+
+    updateProperty(tags, values);
+}
+
+function updateProperty(tags, values) {
+    // read current properties from file
+    const currentProperties = fs.readFileSync('data.properties', 'utf8');
+  
+    // parse current properties into object
+    const parsedProperties = parseProperties(currentProperties);
+  
+    // update properties in object with updated values
+    for (let i = 0; i < tags.length; i++) {
+        if (parsedProperties.hasOwnProperty(tags[i])) {
+            parsedProperties[tags[i]] = values[i];
+        }
+    }
+  
+    // write updated properties object to file
+    fs.writeFileSync('data.properties', formatProperties(parsedProperties));
+  
+    console.log('Properties file updated!');
+  
+    // function to format properties object into string
+    function formatProperties(properties) {
+        let output = '';
+        for (let key in properties) {
+            output += key + '=' + properties[key] + '\n';
+        }
+        return output;
+    }
+}
+
+  
+// function to parse properties string into object
+function parseProperties(propertiesString) {
+  var properties = {};
+  const lines = propertiesString.split('\n');
+  for (let i = 0; i < lines.length; i++) {
+    const parts = lines[i].split('=');
+    if (parts.length >= 2) {
+      const key = parts[0].trim();
+      const value = parts[1].trim();
+      properties[key] = value;
+    }
+  }
+  return properties;
+}
+
 
 function remoteAction(action, id){ ipc.send('driveAction', [action, id]); }
 
